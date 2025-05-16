204675d9ea9b0de59194e9e374d078822eb4b001
Rreze Ejupi <rrezeejupi@gmail.com>
2025-05-17 00:48:13 +0200

    what to do next

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        // TODO: Zevenesoni kete kod me login funksional (krejt nga terminali)
+        // Scanner per me marr username edhe password prej user
+        // Verifiko passwordin duke e hash inputin edhe me krahasu me stored hash
+        // Kur ka login te suksesshem, call JWT token generator
+        //
+
+
         User user = UserDatabase.getUser("alice");
         if (user != null) {
             System.out.println("Username: " + user.getUsername());
