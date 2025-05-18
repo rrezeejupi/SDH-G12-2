4730aa242aa0107dd02ed2c6f851111330af80e8
Rona Kukaj <ronakukaj@gmail.com>
2025-05-18 18:14:19 +0200

    access protected feature

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         // TODO: Zevenesoni kete kod me login funksional (krejt nga terminali)
         // Scanner per me marr username edhe password prej user
         // Verifiko passwordin duke e hash inputin edhe me krahasu me stored hash
         // Kur ka login te suksesshem, call JWT token generator
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("=== SISTEMI I LOGIN-IT ===");
         System.out.print("Shkruaj emrin e përdoruesit: ");
@@ -26,6 +26,11 @@ public class Main {
             System.out.println("Stored Hashed Password: " + user.getHashedPassword());
 
             //call jwt
+                String token = TokenManager.generateToken(username);
+                System.out.println("Token-i juaj JWT: " + token);
+
+                // Thirr funksionin për veçorinë e mbrojtur
+                handleProtectedFeature();
 
             } else {
                 System.out.println("Fjalëkalimi është i pasaktë.");
@@ -34,5 +39,18 @@ public class Main {
             System.out.println("User not found.");
         }
         scanner.close();
+    }
+    private static void handleProtectedFeature() {
+        System.out.println("\n=== Protected Feature ===");
+        System.out.print("Enter your JWT token: ");
+        String token = scanner.nextLine();
+
+        if (TokenManager.verifyToken(token)) {
+            String username = TokenManager.getUsernameFromToken(token);
+            System.out.println("Access granted to user: " + username);
+            System.out.println("Secret data = 42");
+        } else {
+            System.out.println("Access denied! Invalid or expired token.");
+        }
     }
 }
